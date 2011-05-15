67e822fcba7418cef77ebdbec92b887c0faca79a
Joe Hoyle <joehoyle@gmail.com>
2011-05-15 12:25:53 +0100

    Add theme supports for food menus and events

--- themeforce.php
+++ themeforce.php
@@ -9,10 +9,18 @@ define( 'TF_PATH', dirname( __FILE__ ) );
 define( 'TF_URL', str_replace( ABSPATH, get_bloginfo('url') . '/', TF_PATH ) );
 
 //Food Menu
-require_once( TF_PATH . '/food-menu/tf.food-menu.php' );
+if( current_theme_supports( 'tf_food_menu' ) )
+	require_once( TF_PATH . '/food-menu/tf.food-menu.php' );
 
 //Events
-require_once( TF_PATH . '/events/tf.events.php' );
+if( current_theme_supports( 'tf_events' ) )
+	require_once( TF_PATH . '/events/tf.events.php' );
+
+//Widgets
+require_once( TF_PATH . '/newsletter-widget.php' );
+
+//Google Maps
+require_once( TF_PATH . '/tf.googlemaps.shortcodes.php' );
 
 //Options Framework
 define('OF_FILEPATH', STYLESHEETPATH );
@@ -20,3 +28,12 @@ define('OF_DIRECTORY', TF_URL . '/options-framework' );
 	
 require_once ( TF_PATH . '/options-framework/admin/admin-functions.php');		// Custom functions and plugins
 require_once ( TF_PATH . '/options-framework/admin/admin-interface.php');		// Admin Interfaces (options,framework, seo)
+
+/**
+ * Enqueue the admin styles for themeforce features.
+ * 
+ */
+function tf_enqueue_admin_css() {
+	wp_enqueue_style('tf-functions-css', TF_URL . '/assets/css/admin.css');
+}
+add_action('admin_init', 'tf_enqueue_admin_css');
