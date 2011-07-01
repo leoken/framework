c089f6c61db2d4a5720598e47eaca3361bf40632
Noel Tock <hello@noeltock.com>
2011-07-01 15:40:03 +0200

    Start on Schema.org General Settings

---
+++ core_seo/tf.schema.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+Schema.org
+----------
+Spec used here: http://schema.org/FoodEstablishment
+
+This component is for the general details of the business. Events are handled through their own templates.
+
+*/
+
+require_once( dirname( __FILE__ ) . '/tf.schema.admin-options.php' );
+
+// TODO Generate Schema Microdata
+
+// TODO Output Schema Microdata
+
+// TODO Update Schema Microdata Transient
+
+// TODO Refresh Schema Microdata Transient on Options Update
+
+?>
